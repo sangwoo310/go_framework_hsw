9e473d5c788636800b9da7b42f94223ff4cd82e0
hansangwoo <sangwoo310@naver.com>
2021-04-03 20:38:41 +0900

    Make Main Frame

--- hswFramework.go
+++ hswFramework.go
@@ -1,10 +1,24 @@
 package hswFramework
 
-import (
-	"fmt"
-)
+import "github.com/sangwoo310/go_framework_hsw/cmd"
 
-func main() {
-	fmt.Println("Han SangWoo GoLang Framework")
+type IHswFrame interface {
+	Cmd()
+}
 
+type SHswFrame struct {
+	Name string
+}
+
+func NewFrame(*IHswFrame) *SHswFrame {
+
+	s := SHswFrame{}
+
+	return &s
+}
+
+func (h SHswFrame) Cmd() *cmd.CmdStruct {
+	s := cmd.NewApp()
+
+	return s
 }
